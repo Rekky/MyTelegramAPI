fa630543fce3a44d275766cf8ccf75e28944a3fa
Luis Angel Rivas <lrivas.it@hotelbeds.com>
2024-03-27 12:58:50 +0100

    added route get for checking its works

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -1,10 +1,11 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import authController from '../controllers/authController';
 import messageController from '../controllers/messageController';
 import authMiddleware from '../middleware/authMiddleware';
 
 const router = Router();
 
+router.get('/', (req: Request, res: Response) => {res.status(200).send('API Works')});
 router.post('/generate-token', authController.generateToken);
 router.post('/send', authMiddleware, messageController.sendMessage);
 
